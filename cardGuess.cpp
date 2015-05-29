47ed791157b5b35027ece82b63218517181d4af5
eidolonFIRE <C4leb.Johnson@gmail.com>
2015-05-29 10:58:58 -0700

    fixed bugs added more graph data added raw data

--- cardGuess.cpp
+++ cardGuess.cpp
@@ -10,67 +10,103 @@
 using namespace std;
 
 unsigned int histo[52];
-
+unsigned int histoTime[52];
 
 void zeroHisto()
 {
 	for (int t = 0; t < 52; t++)
 	{
 		histo[t] = 0;
+		histoTime[t] = 0;
 	}
 }
 
-int histoGetMaxValue()
+int histoGetMaxValue(unsigned int *data)
 {
 	int retval = 1;
 	for (int t = 0; t < 52; t++)
 	{
-		if (histo[t] > retval) retval = histo[t];
+		if (data[t] > retval) retval = data[t];
 	}
 	return retval;
 }
 
-
-void printHisto()
+int histoGetMaxIndex(unsigned int *data)
 {
-	int height = 20;
-	int scale = histoGetMaxValue();
-	//printf("\x1b[<0>;<0>f");
-	system("cls");
+	int max = histoGetMaxValue(data);
+	for (int t = 51; t > 0; t--)
+	{
+		if (data[t] == max) return t;
+	}
+	return 0;
+}
+
+float histoGetMean(unsigned int *data)
+{
+	unsigned int sum, weighted;
+	for (int t = 0; t < 52; t++)
+	{
+		weighted += data[t] * t;
+		sum += data[t];
+	}
+	return (float)weighted / (float)sum;
+}
+
+void printHistoData(unsigned int *data)
+{
+	printf("Histo Data:\n");
+	for (int t = 0; t < 52; t++) printf("\t%2d:%7d\n",t, data[t]);
+}
+
+void printHisto(int height, unsigned int *data)
+{
+	int scale = histoGetMaxValue(data);
+	//printf("\033[0;0f"); 
+	for (int x = 0; x < 52; x++) printf("-");
+	printf("\n");
 	for (int y = height; y > 0; y--)
 	{
 		for (int x = 0; x < 52; x++)
 		{
-			if (histo[x] >= y * scale / height)
-				printf("\xDB");
-			else
+			if (data[x] <= int((float(y) + 0.5f) * (float)scale / (float)height) || data[x] == 0)
 				printf(" ");
+			else
+				printf("|");
 		}
 		printf("\n");
 	}
 
 	for (int x = 0; x < 52; x++) printf("-");
 	printf("\n");
+	//printHistoData(data);
 }
+
 
 
 int randWeighted(int cc[4])
 {
 	int sum = cc[0] + cc[1] + cc[2] + cc[3];
-	if (sum == 0) sum = 1;
+	if (sum == 0)
+	{
+		return rand()%4;
+	}
 	int randVal = rand()%(sum);
 
 	if (randVal < cc[0]) return 0;
-	else if (randVal >= cc[0] && randVal < cc[0]+cc[1]) return 1;
+	else if (randVal >= cc[0]       && randVal < cc[0]+cc[1]) return 1;
 	else if (randVal >= cc[0]+cc[1] && randVal < cc[0]+cc[1]+cc[2]) return 2;
 	else if (randVal >= cc[0]+cc[1]+cc[2]) return 3;
+	return 0;
 }
 
 
 
 int main(int argc, char **argv)
 {	
+	int runsMax = 100000;
+	system("clear");
 
+	/*
 	//=====================================================
 	//    Test weighting
 	//
@@ -99,8 +135,7 @@ int main(int argc, char **argv)
 	
 	printHisto();
 	printf("Weight Test:\n");
-	system("pause");
-
+*/
 
 
 
@@ -114,21 +149,25 @@ int main(int argc, char **argv)
 	//
 	//-----------------------------------------------------
 	zeroHisto();
-	for (int runs = 0; runs < 100000; runs++)
+	for (int runs = 0; runs < runsMax; runs++)
 	{
 		deck.shuffle();
 		int correct = 0;
 		for (int t = 0; t < 52; t++)
 		{
 			int guess = rand()%4;
-			if (guess == deck.cards[t]) correct++;
+			if (guess == deck.cards[t])
+			{
+				correct++;
+				histoTime[t]++;
+			}
 		}
 		histo[correct]++;
 	}
 	
-	printHisto();
-	printf("Blind:\n");
-	system("pause");
+	printHisto(10, histo);
+	printHisto(5, histoTime);
+	printf("Blind: %.4f         \n", histoGetMean(histo));
 	
 
 
@@ -138,7 +177,7 @@ int main(int argc, char **argv)
 	//-----------------------------------------------------
 	zeroHisto();
 		
-	for (int runs = 0; runs < 10000; runs++)
+	for (int runs = 0; runs < runsMax; runs++)
 	{
 		deck.shuffle();
 		int correct = 0;
@@ -147,15 +186,19 @@ int main(int argc, char **argv)
 		for (int t = 0; t < 52; t++)
 		{
 			int guess = randWeighted(cc);
-			if (guess == deck.cards[t]) correct++;
+			if (guess == deck.cards[t]) 
+			{
+				correct++;
+				histoTime[t]++;
+			}
 			cc[deck.cards[t]]--;
 		}
 		histo[correct]++;
 	}
 	
-	printHisto();
-	printf("Basic Counting:\n");
-	system("pause");
+	printHisto(10, histo);
+	printHisto(5, histoTime);
+	printf("Basic Counting: %.4f         \n", histoGetMean(histo));
 
 
 	//=====================================================
@@ -164,7 +207,7 @@ int main(int argc, char **argv)
 	//-----------------------------------------------------
 	zeroHisto();
 		
-	for (int runs = 0; runs < 10000; runs++)
+	for (int runs = 0; runs < runsMax; runs++)
 	{
 		deck.shuffle();
 		int correct = 0;
@@ -173,27 +216,31 @@ int main(int argc, char **argv)
 		for (int t = 0; t < 52; t++)
 		{
 			int guess = randWeighted(cc);
-			if (guess == deck.cards[t]) correct++;
+			if (guess == deck.cards[t])
+			{
+				correct++;
+				histoTime[t]++;
+			}
 			cc[0]++;
 			cc[1]++;
 			cc[2]++;
 			cc[3]++;
-			cc[deck.cards[t]] = 0;
+			cc[deck.cards[t]] = 1;
 		}
 		histo[correct]++;
 	}
 	
-	printHisto();
-	printf("Depreciating History:\n");
-	system("pause");
+	printHisto(10, histo);
+	printHisto(5, histoTime);
+	printf("Depreciating History: %.4f         \n", histoGetMean(histo));
 
 
 	//=====================================================
-	//    Basic card counting + Depreciating history
+	//    Basic card counting * Depreciating history
 	//
 	//-----------------------------------------------------
 	zeroHisto();
-	for (int runs = 0; runs < 10000; runs++)
+	for (int runs = 0; runs < runsMax; runs++)
 	{
 		deck.shuffle();
 		int correct = 0;
@@ -208,20 +255,24 @@ int main(int argc, char **argv)
 			ccF[2] = cc[2] * cc2[2];
 			ccF[3] = cc[3] * cc2[3];
 			int guess = randWeighted(ccF);
-			if (guess == deck.cards[t]) correct++;
+			if (guess == deck.cards[t])
+			{
+				correct++;
+				histoTime[t]++;
+			}
 			cc[0]++;
 			cc[1]++;
 			cc[2]++;
 			cc[3]++;
-			cc[deck.cards[t]] = 0;
+			cc[deck.cards[t]] = 1;
 			cc2[deck.cards[t]]--;
 		}
 		histo[correct]++;
 	}
 	
-	printHisto();
-	printf("Basic card counting + Depreciating history:\n");
-	system("pause");
+	printHisto(10, histo);
+	printHisto(5, histoTime);
+	printf("Basic card counting * Depreciating history: %.4f         \n", histoGetMean(histo));
 
 
 
